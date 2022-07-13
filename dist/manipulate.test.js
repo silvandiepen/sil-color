7e4c8aeb48fbf5af3b1f385b943f99827fd04f29
Sil van Diepen <me@sil.mt>
2022-07-13 15:18:24 +0200

    wip:add tests for setLightness

--- dist/manipulate.test.js
+++ dist/manipulate.test.js
@@ -59,4 +59,43 @@ describe("to Darkened color", () => {
         });
     });
 });
+const setLightnessTest = [
+    {
+        input: { r: 0, g: 0, b: 0 },
+        amount: 25,
+        output: { r: 64, g: 64, b: 64 },
+    },
+    {
+        input: { r: 127, g: 127, b: 127 },
+        amount: 50,
+        output: { r: 128, g: 128, b: 128 },
+    },
+    {
+        input: { h: 0, s: 0, l: 0 },
+        amount: 75,
+        output: { h: 0, s: 0, l: 75 },
+    },
+    {
+        input: { h: 0, s: 0, l: 10 },
+        amount: 100,
+        output: { h: 0, s: 0, l: 100 },
+    },
+    {
+        input: "#000000",
+        amount: 50,
+        output: "#808080",
+    },
+    {
+        input: "#FF0000",
+        amount: 25,
+        output: "#800000",
+    },
+];
+describe("Set Lightened color", () => {
+    setLightnessTest.forEach((value) => {
+        it(`should convert any color to a lighter version`, () => {
+            expect((0, manipulate_1.setLightness)(value.input, value.amount)).toEqual(value.output);
+        });
+    });
+});
 //# sourceMappingURL=manipulate.test.js.map
--- src/manipulate.test.ts
+++ src/manipulate.test.ts
@@ -1,5 +1,5 @@
 import { lighten, darken, setLightness } from "./manipulate";
-import { HSL, RGB } from "./types";
+import { HEX, HSL, RGB } from "./types";
 
 const lightenColors = [
   {
@@ -58,6 +58,50 @@ describe("to Darkened color", () => {
   darkenColors.forEach((value) => {
     it(`should convert to RGB string - ${JSON.stringify(value.input)}`, () => {
       expect(darken(value.input, value.amount)).toEqual(value.output);
+    });
+  });
+});
+const setLightnessTest: {
+  input: RGB | HSL | HEX;
+  amount: HSL["l"];
+  output: RGB | HSL | HEX;
+}[] = [
+  {
+    input: { r: 0, g: 0, b: 0 } as RGB,
+    amount: 25,
+    output: { r: 64, g: 64, b: 64 } as RGB,
+  },
+  {
+    input: { r: 127, g: 127, b: 127 } as RGB,
+    amount: 50,
+    output: { r: 128, g: 128, b: 128 } as RGB,
+  },
+  {
+    input: { h: 0, s: 0, l: 0 } as HSL,
+    amount: 75,
+    output: { h: 0, s: 0, l: 75 } as HSL,
+  },
+  {
+    input: { h: 0, s: 0, l: 10 } as HSL,
+    amount: 100,
+    output: { h: 0, s: 0, l: 100 } as HSL,
+  },
+  {
+    input: "#000000" as HEX,
+    amount: 50,
+    output: "#808080" as HEX,
+  },
+  {
+    input: "#FF0000" as HEX,
+    amount: 25,
+    output: "#800000" as HEX,
+  },
+];
+
+describe("Set Lightened color", () => {
+  setLightnessTest.forEach((value) => {
+    it(`should convert any color to a lighter version`, () => {
+      expect(setLightness(value.input, value.amount)).toEqual(value.output);
     });
   });
 });
